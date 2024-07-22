7b846700763bb385d6702a5c45ba1616dced512c
Zoody <reopard506@gmail.com>
2024-07-22 22:29:30 +0900

    [✨FEAT]      : 동적 env 환경 구성
      - dotenv 를 통한 동적 env 사용

--- apps/flashlight/src/main.ts
+++ apps/flashlight/src/main.ts
@@ -1,5 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { FlashlightModule } from './flashlight.module';
+import { NestFactory } from "@nestjs/core";
+import { FlashlightModule } from "./flashlight.module";
+import * as dotenv from "dotenv";
+import * as path from "path";
+
+dotenv.config({
+  path: path.resolve(
+    process.env.NODE_ENV === "development" ? ".env" : ".env.stage",
+  ),
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(FlashlightModule);
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,14 @@ module.exports = {
       name: "flashlight",
       script: "dist/apps/flashlight/main.js",
       instances: 1,
+      env: {
+        port: "4000",
+        NODE_ENV: "development",
+      },
+      env_stage: {
+        port: "4000",
+        NODE_ENV: "stage",
+      },
     },
   ],
 };
